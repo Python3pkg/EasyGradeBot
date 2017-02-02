89488a2598f6d99b3d396b846fc3a94ee58ff627
SeanPianka <me@seanpianka.com>
2017-02-02 14:49:52 -0500

    correctly downloading latest submission, optional information to be saved

--- easygrade/easygrade.py
+++ easygrade/easygrade.py
@@ -1,5 +1,6 @@
 import json
 import time
+import os
 import sys
 from pprint import pprint
 from urllib.parse import parse_qs, urlparse
@@ -36,31 +37,18 @@ class EasyGradeBot(FSUBot):
         wait = ui.WebDriverWait(self.dr, 10)
         self.SLEEP_TIME = 0.5
 
-        with open('pages.json') as f:
-            pages = json.load(f)
-
-        # must be a list
-        portal_tab_json = [pages['pages'][0]] # not a list
-        blackboard_tab_json = pages['pages'][1:] # already a list
-        bot.navigate(json_list=portal_tab_json)
-
+        bot._navigate(title="BlackBoard", xpath="//*[@id=\"link_icon_197\"]")
         blackboard_tab = [h for h in self.dr.window_handles if h != portal_tab][0]
-        ignored_tabs = [portal_tab]
-
         self.dr.switch_to_window(blackboard_tab)
-        bot.navigate(json_list=blackboard_tab_json)
-
-	# should now be at list of all grades
-
-        # useful for after switch to course_name
-        #course_id = get_query_field(self.dr.current_url, "course_id")[0]
 
         smartview_listing_url = EasyGradeBot.SMARTVIEWS_URL.format(course_id)
         self.dr.get(smartview_listing_url)
+        wait.until(lambda driver: driver.find_element_by_xpath(
+            '//*[@id="nav"]/li/a'
+        ))
 
         # getting smartview URLs
         tree = html.fromstring(self.page_source)
-
         smartview_table_rows_xpath = '//*[@id="listContainer_databody"]/tr'
         smartview_rows = tree.xpath(smartview_table_rows_xpath)
         smartview_ids = []
@@ -77,7 +65,9 @@ class EasyGradeBot(FSUBot):
         for smartview_id in smartview_ids:
             # get and wait for page load (hopefully 3 is enough)
             self.dr.get(EasyGradeBot.SMARTVIEW_URL.format(course_id, smartview_id))
-            time.sleep(3)
+            wait.until(lambda driver: driver.find_element_by_xpath(
+                '//*[@id="cell_0_3"]/div/div[1]/div/a'
+            ))
 
             # create tree to parse
             smartview_tree = html.fromstring(self.page_source)
@@ -96,9 +86,8 @@ class EasyGradeBot(FSUBot):
 
             # submissions will collect URLs to download the assignments
             students = []
-            student_table_rows_css_selector = '#table1 > tbody > tr'
             student_rows = smartview_tree.cssselect(
-                student_table_rows_css_selector
+                '#table1 > tbody > tr'
             )
 
             # now, let's get all of the items in the necessary columns
@@ -119,21 +108,15 @@ class EasyGradeBot(FSUBot):
                 }
 
                 for column_id, column_name in column_ids.items():
-                    dropdown_btn = self.dr.find_element_by_id(
-                        'cmlink_{}{}'.format(row_id, column_id)
-                    )
                     student['Submissions'].append({
                         'Title': column_name,
                         'Row': row_id,
                         'Column': column_id,
-                        'Dropdown': dropdown_btn
                     })
-
                 students.append(student)
 
 
             cell_id = "cell_{}_{}"
-
             for student in students:
                 for submission in student['Submissions']:
                     cell = self.dr.find_element_by_css_selector(
@@ -141,65 +124,62 @@ class EasyGradeBot(FSUBot):
                             submission['Row'], submission['Column']
                         )
                     ).click()
-                    time.sleep(1)
-                    dropdown_btn = self.dr.find_element_by_css_selector(
+                    time.sleep(0.5)
+                    # click dropdown to make "View Grade Details" element visible
+                    self.dr.find_element_by_css_selector(
                         'a#cmlink_{}{}'.format(
                             submission['Row'], submission['Column']
                         )
-                    )
-                    dropdown_btn.click()
-                    time.sleep(1)
-                    view_grade_details = [
-                        a for a in self.dr.find_elements_by_xpath(
-                            '//*[@id="context_menu_tag_item1_{}{}"]'.format(
-                                submission['Row'], submission['Column']
-                            )
-                        ) if "View Grade Details" in a.get_attribute("title")
-                    ][0]
+                    ).click()
+                    time.sleep(0.5)
+
+                    # navigate to view grade details
+                    [a for a in self.dr.find_elements_by_xpath(
+                        '//*[@id="context_menu_tag_item1_{}{}"]'.format(
+                            submission['Row'], submission['Column']
+                        )
+                    ) if "View Grade Details" in a.get_attribute("title")][0].click()
 
                     # entering grade details page
-                    view_grade_details.click()
-                    attempt_rows = len(self.dr.find_elements_by_xpath(
+                    latest_attempt_row = len(self.dr.find_elements_by_xpath(
                         '//*[@id="attemptsTable"]/tbody/tr[contains(@id,"attemptRow")]'
-                    ))
+                    )) - 1
 
                     attempt_url = "https://campus.fsu.edu/webapps/assignment/gradeAssignmentRedirector?anonymousMode=false&course_id={}&attempt_id={}"
                     attempt_row_xpath = '//*[@id="attemptsTable"]/tbody/tr[contains(@id,"attemptRow{}")]'
 
-                    for attempt_row_num in range(attempt_rows):
-                        attempt_row = self.dr.find_element_by_xpath(
-                            attempt_row_xpath.format(attempt_row_num)
+                    attempt_row = self.dr.find_element_by_xpath(
+                        attempt_row_xpath.format(latest_attempt_row)
+                    )
+                    attempt_row_id = attempt_row.get_attribute("id")
+
+                    submit_date = attempt_row.find_element_by_xpath(
+                        '//*[@id="{}"]/td[1]/div'.format(attempt_row_id)
+                    ).text
+
+                    attempt_id = attempt_row.find_element_by_xpath(
+                        '//*[@id="{}"]/td[6]/div/a[1]'.format(
+                            attempt_row_id
                         )
-                        attempt_row_id = attempt_row.get_attribute("id")
+                    ).get_attribute("onclick").split("'")[1].split("'")[0]
 
-                        submit_date = attempt_row.find_element_by_xpath(
-                            '//*[@id="{}"]/td[1]/div'.format(attempt_row_id)
-                        ).text
-
-                        attempt_id = attempt_row.find_element_by_xpath(
-                            '//*[@id="{}"]/td[6]/div/a[1]'.format(
-                                attempt_row_id
-                            )
-                        ).get_attribute("onclick").split("'")[1].split("'")[0]
-
-                        self.dr.get(attempt_url.format(course_id, attempt_id))
-                        wait.until(lambda driver: driver.find_element_by_xpath(
-                            '//*[@id="downloadPanelButton"]'
-                        ))
-                        download_btn = self.dr.find_element_by_xpath(
-                            '//*[@id="downloadPanelButton"]'
-                        )
-                        download_btn.click()
-                        time.sleep(2)
-                        self.dr.execute_script('window.history.go(-1)')
-
-
-                    time.sleep(1)
+                    self.dr.get(attempt_url.format(course_id, attempt_id))
+                    wait.until(lambda driver: driver.find_element_by_xpath(
+                        '//*[@id="downloadPanelButton"]'
+                    ))
+                    download_btn = self.dr.find_element_by_xpath(
+                        '//*[@id="downloadPanelButton"]'
+                    )
+                    download_btn.click()
                     self.dr.execute_script('window.history.go(-1)')
-                    time.sleep(1)
 
-
-            time.sleep(5)
+                    self.dr.execute_script('window.history.go(-1)')
+                    wait.until(lambda driver: driver.find_element_by_css_selector(
+                        'td#cell_{}_{}'.format(
+                            submission['Row'], submission['Column']
+                        )
+                    ))
+            time.sleep(1)
 
         sys.exit()
 
@@ -208,23 +188,22 @@ if __name__ == '__main__':
     mbpath = '../../../../../../../../../../../../usr/local/bin/chromedriver'
     calderapath = '../drivers/chromedriver'
 
-    #profile = webdriver.ChromeOptions()
-    #prefs = {"download.default_directory" : "/home/sean/easygradeassignments"}
-    #profile.add_experimental_option("prefs", prefs)
-    bot = EasyGradeBot(
-        fsuid=fsuid, fsupw=fsupw,
-        browser={
-            'title': 'chrome',
-            'path' :mbpath,
-            'profile': None
-        }
+    chrome_options = webdriver.ChromeOptions()
+    download_dir = os.path.join(os.path.expanduser("~"), "Downloads/assignments")
+    prefs = {"download.default_directory" : download_dir}
+    chrome_options.add_experimental_option("prefs", prefs)
+    driver = webdriver.Chrome(
+        executable_path='/usr/local/bin/chromedriver',
+        chrome_options=chrome_options
     )
 
-    with open('download.json') as f:
-        smartview_json = json.load(f)
+    bot = EasyGradeBot(
+        fsuid=fsuid, fsupw=fsupw,
+        driver=driver,
+    )
 
     bot.main(
-        smartview_json['course_id'], # eventually switch this to course name
+        '_6430453_1', # eventually switch this to course name
         ["04_Pianka", "07_Pianka", "16_Pianka", "17_Pianka"],
         ["Homework 1"]
     )
